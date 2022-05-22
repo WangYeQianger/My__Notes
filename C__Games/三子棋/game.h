7a305401e46e46ad656357a0865bcd334062de7b
王业强 <104551090+WangYeQianger@users.noreply.github.com>
2022-05-22 17:35:40 +0800

    Update game.h

--- C__Games/三子棋/game.h
+++ C__Games/三子棋/game.h
@@ -5,6 +5,7 @@
 #define ROW 3
 #define COL 3
 
+
 void InitBoard(char board[ROW][COL],int row,int col);
 void DisplayBoard(char board[ROW][COL], int row, int col);
 void PlayerMove(char board[ROW][COL], int row, int col);
